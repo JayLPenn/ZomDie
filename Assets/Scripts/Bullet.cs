0c6232b8d51190910324f450015b9497bacfe6b9
JayLPenn <JamesLockyerPenn@gmail.com>
2023-03-12 10:16:43 +0800

    20230312 - 10:16

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,17 +6,28 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
     Rigidbody2D rigidBody;
+    ParticleSystem pSystem;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.AddForce(transform.right * speed, ForceMode2D.Impulse);
+
+        pSystem = GameObject.Find("EmitterDestruction").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var pSE = pSystem.emission;
+        pSE.enabled = true;
+        pSystem.Play();
+        //Destroy(gameObject);
     }
 }
